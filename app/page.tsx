4ff11db78d5ab632cdcecb4005fa2ab16fa5e9b3
PiotrZettt <piotrzielinski@o2.pl>
2023-11-10 19:43:05 +0000

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
 import Image from 'next/image'
-import { Auth } from 'aws-amplify';
-import { Amplify } from 'aws-amplify';
 
-import awsconfig from '../src/aws-exports';
-Amplify.configure(awsconfig);
 
 export default async function Home() {
-
-  try {
-    const user = await Auth.currentAuthenticatedUser({
-      bypassCache: false // Optional and is false by default. If set to true, this call 
-      // will send a request to Cognito to get the latest user data.
-    });
-    console.log(user);
-  } catch(err) {
-    console.log(err);
-  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
