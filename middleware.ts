19aa7ef544831c60d5960a9ce4cf7ffe5c4e62d2
PiotrZettt <piotrzielinski@o2.pl>
2023-11-10 07:06:00 +0000

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default NextAuth(authConfig).auth;
  
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: '/dashboard/:path*'
+  matcher: ['/dashboard/:path*', '/api/:path*']
 };
