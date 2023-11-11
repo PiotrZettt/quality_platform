7469e752ecc71d2b65e1dbb79b5c11060b0a6060
PiotrZettt <piotrzielinski@o2.pl>
2023-11-11 18:17:19 +0000

    Update Auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,15 @@
 import type { NextAuthConfig } from 'next-auth';
+import CognitoProvider from "next-auth/providers/cognito";
+
  
 export const authConfig = {
-  providers: [],
+  providers: [
+    CognitoProvider({
+      clientId: process.env.COGNITO_CLIENT_ID,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET,
+      issuer: process.env.COGNITO_ISSUER,
+    })
+  ],
   pages: {
     signIn: '/login',
   },
