2895751ee8e9af91039eb1d0fdcea6f0c3a862b1
PiotrZettt <piotrzielinski@o2.pl>
2023-11-11 18:29:57 +0000

    Update Auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
+
 import { authConfig } from './auth.config';
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
+import CognitoProvider from "next-auth/providers/cognito";
 import bcrypt from 'bcrypt'
 
 async function getUser(email: string): Promise<User | undefined> {
@@ -20,25 +21,11 @@ async function getUser(email: string): Promise<User | undefined> {
  
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [
-    Credentials({
-        async authorize(credentials) {
-          const parsedCredentials = z
-            .object({ email: z.string().email(), password: z.string().min(6) })
-            .safeParse(credentials);
-
-        if (parsedCredentials.success) {
-        const { email, password } = parsedCredentials.data;
-        const user = await getUser(email);
-        if (!user) return null;
-        const passwordsMatch = await bcrypt.compare(password, user.password);
-
-        if (passwordsMatch) return user;
-        }
- 
-        console.log('Invalid credentials');
-        return null;
-      },
-    }),
+  providers: [CognitoProvider({
+    clientId: process.env.COGNITO_CLIENT_ID,
+    clientSecret: process.env.COGNITO_CLIENT_SECRET,
+    issuer: process.env.COGNITO_ISSUER,
+  })
+    
   ],
 });
